1bbb9be1415b43c2bc321af60c506286de1ae54d
Richard Rojas <richardroj@icloud.com>
2024-10-16 20:59:13 +0200

    Screen

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,20 +1,46 @@
 import { Routes } from '@angular/router';
+import { AuthGuard } from './business/core/guards/auth.guard';
+import { AuthenticatedGuard } from './business/core/guards/authenticated.guard';
 
 export const routes: Routes = [
 
     {
-        path: 'dashboard',
-        loadComponent: () => import('./dashboard/dashboard.component'),
+        path: '',
+        loadComponent: () => import('./shared/components/layout/layout.component'),
         children: [
+            {
+                path: 'dashboard',
+                loadComponent: () => import('./business/dashboard/dashboard.component'),
+                canActivate: [AuthGuard]
+            },
+            {
+                path: 'profile',
+                loadComponent: () => import('./business/profile/profile.component'),
+                canActivate: [AuthGuard]
+            },
+            {
+                path: 'tables',
+                loadComponent: () => import('./business/tables/tables.component'),
+                canActivate: [AuthGuard]
+            },
+            {
+                path: 'associates',
+                loadComponent: () => import('./business/associates/associates.component'),
+                canActivate: [AuthGuard]
+            },
+            {
+                path: '',
+                redirectTo: 'dashboard',
+                pathMatch: 'full'
+
+            }
 
         ]
     },
     {
         path: 'login',
-        loadComponent: () => import('./login/pages/login-page.component'),
-        children: [
-
-        ]
+        loadComponent: () => import('./business/auth/login/login.component'),
+        canActivate: [AuthenticatedGuard]
     },
     {
         path:'**',
